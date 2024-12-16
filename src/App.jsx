41d452168d921d171af6f791c79d6f0012c66946
fahim484 <fahim.faysal.webdeveloper@gmail.com>
2024-12-16 10:13:17 +0600

    tailwind css added successfully

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import './App.css'
 const App = () => {
   return (
     <>
-      <h1>My Feedback</h1>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </>
   )
 }

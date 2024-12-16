4783716b8941b572689e5acf1b6c08e6c410982e
fahim484 <fahim.faysal.webdeveloper@gmail.com>
2024-12-16 11:14:24 +0600

    google fonts added successfully

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,12 @@ export default {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ["Lexend", "sans-serif"],
+        body: ["Roboto", "sans-serif"],
+      },
+    },
   },
   darkMode: "class",
   plugins: [nextui()],

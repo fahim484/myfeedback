e11db95a8e2284fe53f85ca9dd22d2d6d38af407
fahim484 <fahim.faysal.webdeveloper@gmail.com>
2024-12-19 12:17:57 +0600

    tailwind.config.js updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 import flowbite from "flowbite-react/tailwind";
-import { nextui } from "@nextui-org/react";
+const { nextui } = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
-const config = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-    ...flowbite.content(), // Spread for ES Module compatibility
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -34,5 +34,3 @@ const config = {
   darkMode: "class",
   plugins: [nextui(), flowbite.plugin()],
 };
-
-export default config;
